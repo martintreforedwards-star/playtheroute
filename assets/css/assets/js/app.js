cecfd40ef13c21d830b03edcbf75546656227645
martintreforedwards-star <martintreforedwards@gmail.com>
2026-05-06 12:28:07 +0100

    Update app.js

--- assets/css/assets/js/app.js
+++ assets/css/assets/js/app.js
@@ -1,1 +1,69 @@
-console.log("app loaded");
+let selectedCell = null;
+
+let tickets = 3;
+
+const validStations = [
+  "Brighton",
+  "Blackfriars",
+  "East Croydon",
+  "London Bridge",
+  "St Albans"
+];
+
+function selectCell(cell) {
+
+  if (selectedCell) {
+    selectedCell.classList.remove("selected");
+  }
+
+  selectedCell = cell;
+
+  selectedCell.classList.add("selected");
+}
+
+function placeStation() {
+
+  const input =
+    document.getElementById("stationInput");
+
+  const value =
+    input.value.trim();
+
+  if (!selectedCell) {
+    alert("Select a cell first");
+    return;
+  }
+
+  if (validStations.includes(value)) {
+
+    selectedCell.textContent = value;
+
+    selectedCell.classList.remove("selected");
+    selectedCell.classList.add("correct");
+
+  } else {
+
+    selectedCell.classList.add("incorrect");
+
+    loseTicket();
+  }
+
+  input.value = "";
+}
+
+function loseTicket() {
+
+  tickets--;
+
+  const ticketDisplay =
+    document.getElementById("tickets");
+
+  ticketDisplay.textContent =
+    "🎫 ".repeat(tickets);
+
+  if (tickets <= 0) {
+
+    alert("Out of tickets!");
+
+  }
+}
